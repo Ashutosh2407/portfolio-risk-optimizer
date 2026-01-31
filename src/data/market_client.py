29f8b5f99795c76509e1b35f42c9329f3e1ecc3f
ashutosh2407 <adwagh24@gmail.com>
2026-01-31 14:46:15 -0500

    added get snapshot api

--- src/data/market_client.py
+++ src/data/market_client.py
@@ -58,7 +58,7 @@ class MarketDataClient:
                 'sort':"asc",
                 'limit':120
             }
-            endpoint = f"{self.base_url}v2/aggs/ticker/{symbol}/range/{multiplier}/{timespan}/{start_date}/{end_date}?adjusted={params['adjusted']}&sort={params['sort']}&limit={params['limit']}&apikey={self.api_key}"
+            endpoint = f"{self.base_url}/v2/aggs/ticker/{symbol}/range/{multiplier}/{timespan}/{start_date}/{end_date}?adjusted={params['adjusted']}&sort={params['sort']}&limit={params['limit']}&apikey={self.api_key}"
             print(endpoint)
             response = self.session.get(endpoint,timeout=10)
             response.raise_for_status()
@@ -73,6 +73,33 @@ class MarketDataClient:
             return []
 
 
-# m = MarketDataClient(api_key=os.getenv("API_KEY"), base_url= "https://api.massive.com/")
-# s = m.get_ohlc("AAPL",1,"day","2025-11-03","2025-11-28")
-# print(s)
+        # m = MarketDataClient(api_key=os.getenv("API_KEY"), base_url= "https://api.massive.com/")
+        # s = m.get_ohlc("AAPL",1,"day","2025-11-03","2025-11-28")
+        # print(s)
+
+    def get_ticker_snapshot(self,symbol):  #NEED PAID VERSION
+        """
+        Get current market snapshot for multiple symbols
+        
+        Args:
+            symbols: List of ticker symbols
+        
+        Returns:
+            Dictionary with current quotes
+        """
+        try:
+            endpoint = f"{self.base_url}/v2/snapshot/locale/us/markets/stocks/tickers/{symbol}?apiKey={self.api_key}"
+            print(endpoint)
+            response = self.session.get(endpoint,timeout = 10)
+            response.raise_for_status()
+
+            data= response.json()
+            logger.info(f"Fetched snapshot for {symbol} ticker.")
+
+            return data
+
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Unable to fetche snapshot for {symbol}: {e}")
+            return {}
+        
+
