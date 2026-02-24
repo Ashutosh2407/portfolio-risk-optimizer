c35663bf18575f9374de37718124ec3ef69b8812
ashutosh2407 <adwagh24@gmail.com>
2026-02-24 18:37:57 -0500

    created test for processor.py

---
+++ tests/test_processor.py
@@ -0,0 +1,65 @@
+import sys
+import os
+
+# Get the absolute path to the project root
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(current_dir)
+
+# Add to path if not already there
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
+from src.data.processor import MarketDataProcessor, ProcessorConfig
+from src.data.db_client import DatabaseClient
+from unittest.mock import MagicMock
+import numpy as np
+import pandas as pd
+import pytest
+
+class TestProcessor:
+    """
+    Unit Test for Processor
+    """
+    @pytest.fixture
+    def sample_prices(self):
+        """
+        Create sample prices for testing.
+        """
+        np.random.seed(42)
+        rng = np.random.default_rng()
+        dates = pd.date_range('2023-01-01', periods = 252, freq = 'D')
+        prices = pd.DataFrame({
+            'AAPL':rng.integers(1,500,252),
+            'GOOGL':rng.integers(1,500,252),
+            'MSFT':rng.integers(1,500,252)
+        }, index = dates)
+
+        return prices
+    
+    @pytest.fixture
+    def mock_db(self):
+        db = MagicMock(spec=DatabaseClient)
+        return db
+    
+    @pytest.fixture
+    def processor(self,mock_db):
+        config = ProcessorConfig(missing_prices="ffill")
+        return MarketDataProcessor(db = mock_db,config = config)
+
+
+    def test_return_matrix_has_no_nan(self,processor,sample_prices):
+        returns = processor.generate_returns_matrix(sample_prices)
+        assert returns is not None
+        assert not returns.empty
+        assert not returns.isnull().values.any(), "Returns must not contain NaN values"
+
+    def test_generate_risk_metrics(self,processor,sample_prices):
+        returns = processor.generate_returns_matrix(sample_prices)
+        risk_metrics = processor.generate_risk_matrix(returns)
+        cov_daily = risk_metrics["cov_daily"]
+        cov_annual = risk_metrics["cov_annual"]
+        corr = risk_metrics["corr"]
+
+        assert np.array_equal(cov_daily,cov_daily.T), "Covariance matrix must be symmetrical."
+        assert np.array_equal(cov_annual,cov_annual.T), "Covariance matrix must be symmetrical."
+        assert np.array_equal(corr,corr.T), "Correlation matrix must be symmetrical."
